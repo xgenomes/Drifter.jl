c9f23389da46bfd26f03df017fcf9628a1781213
Nick Boyd <nickboyd@gmail.com>
2020-10-29 15:46:51 -0400

    3D

--- src/Drifter.jl
+++ src/Drifter.jl
@@ -1,54 +1,50 @@
 module Drifter
-using StaticArrays
-using LinearAlgebra: dot, norm
 using Transducers
 using Optim
 
 export drift_estimation
 
-const P2D = SVector{2,Float64}
-
 include("celllists.jl")
 
-struct FrameToFrameShift
+struct FrameToFrameShift{N}
     N_matches :: Int64
-    average_shift :: P2D
+    average_shift :: NTuple{N, Float64}
 end
 
-struct Frame
-    points :: Vector{P2D}
-    drift :: P2D
-    celllist :: CellList
+struct Frame{N}
+    points :: Vector{NTuple{N, Float64}}
+    drift :: NTuple{N, Float64}
+    celllist :: CellList{N}
 end
 
-function compute_ave_offset(f1 :: Frame, f2 :: Frame)
-    sum_offsets = SVector(0.0,0.0)
+function compute_ave_offset(f1 :: Frame{N}, f2 :: Frame{N}) where {N}
+    sum_offsets = ntuple(i-> 0.0, Val(N))
     num_matches = 0
     for p in f1.points
-        flag, n = neighbor(f2.celllist, p - f1.drift + f2.drift)
+        flag, n = neighbor(f2.celllist, p .- f1.drift .+ f2.drift)
         if flag
-            sum_offsets += n - p
+            sum_offsets = sum_offsets .+ n .- p
             num_matches += 1
         end
     end
-    FrameToFrameShift(num_matches, sum_offsets/num_matches)
+    FrameToFrameShift(num_matches, sum_offsets./num_matches)
 end
 
 function indexpairs(n_frames, offsets)
     ((i,o) for i in 1:n_frames-1, o in offsets) |> Map(((i,o),) -> (i,i+o)) |>
-        Filter(((i,j),) -> 1 <= j <= n_frames) |> collect
+        Filter(((i,j),) -> 1 <= j <= n_frames) |> collect |> unique
 end
 
-function cached_link(old_value, i,j, frames, min_dist_cache, old_drift, new_drift)
-    if norm((old_drift[i] - old_drift[j]) - (new_drift[i]-new_drift[j])) < min_dist_cache
+function cached_link(old_value, i,j, frames, min_sq_dist_cache, old_drift, new_drift)
+    if _squared_dist(old_drift[i] .- old_drift[j],new_drift[i].-new_drift[j]) < min_sq_dist_cache^2
         (i,j,old_value)
     else
         (i,j,compute_ave_offset(frames[i], frames[j]))
     end
 end
 
-struct DriftLoss
-    drift_pairs :: Vector{Tuple{Int64,Int64, FrameToFrameShift}}
+struct DriftLoss{N}
+    drift_pairs :: Vector{Tuple{Int64,Int64, FrameToFrameShift{N}}}
     lambda :: Float64
 end
 
@@ -88,16 +84,25 @@ function loss_and_gradient!(g, l :: DriftLoss, x :: Vector{Float64}, :: Val{I}) 
     r
 end
 
-function minimize_wrt_drift(l, x_init = zeros(2*l.frame_max))
+function minimize_wrt_drift(l, drift :: Vector{NTuple{2, Float64}})
     #TODO: Use a preconditioner here? Especially when lambda is large...
-    res_x = Optim.optimize(Optim.only_fg!((F,G,x)->loss_and_gradient!(G, l, x, Val{1}())), getindex.(x_init,1)[2:end], method = Optim.LBFGS(m=17), g_tol=1E-10)
-    res_y = Optim.optimize(Optim.only_fg!((F,G,x)->loss_and_gradient!(G, l, x, Val{2}())), getindex.(x_init,2)[2:end], method = Optim.LBFGS(m=17), g_tol=1E-10)
-    pushfirst!([P2D(x,y) for (x,y) in zip(res_x.minimizer,res_y.minimizer)], P2D(0.0,0.0))
+    res_x = Optim.optimize(Optim.only_fg!((F,G,x)->loss_and_gradient!(G, l, x, Val{1}())), getindex.(drift,1)[2:end], method = Optim.LBFGS(m=17), g_tol=1E-10)
+    res_y = Optim.optimize(Optim.only_fg!((F,G,x)->loss_and_gradient!(G, l, x, Val{2}())), getindex.(drift,2)[2:end], method = Optim.LBFGS(m=17), g_tol=1E-10)
+    pushfirst!(map(tuple, res_x.minimizer, res_y.minimizer), (0.0,0.0))
+end
+
+function minimize_wrt_drift(l, drift :: Vector{NTuple{3, Float64}})
+    #TODO: Use a preconditioner here? Especially when lambda is large...
+    res_x = Optim.optimize(Optim.only_fg!((F,G,x)->loss_and_gradient!(G, l, x, Val{1}())), getindex.(drift,1)[2:end], method = Optim.LBFGS(m=17), g_tol=1E-10)
+    res_y = Optim.optimize(Optim.only_fg!((F,G,x)->loss_and_gradient!(G, l, x, Val{2}())), getindex.(drift,2)[2:end], method = Optim.LBFGS(m=17), g_tol=1E-10)
+    res_z = Optim.optimize(Optim.only_fg!((F,G,x)->loss_and_gradient!(G, l, x, Val{3}())), getindex.(drift,3)[2:end], method = Optim.LBFGS(m=17), g_tol=1E-10)
+
+    pushfirst!(map(tuple, res_x.minimizer, res_y.minimizer, res_z.minimizer), (0.0,0.0, 0.0))
 end
 
 
 """
-Estimate the 2D drift for the vector of vectors of points in `localizations`
+Estimate the drift for the vector of vectors of points in `localizations`
 (each vector of points in `localizations` is the localizations in a single frame).
 
 `frame_offsets :: Vector{Int64}` contains the offsets `o` s.t. `localizations[i]` should be compared with `localizations[i+o]` when
@@ -116,14 +121,16 @@ drift ↦ OT(drift, offsets) + λ * ||D*drift||^2
 
 where D is the discrete second derivative.
 """
-function drift_estimation(localizations :: Vector{Vector{P2D}}, frame_offsets :: Vector{Int64}, max_dist, max_iters; drift = [P2D(0.0, 0.0) for _ in 1:length(localizations)], lambda = 0.0, recompute_max_dist = 1E-3)
+function drift_estimation(localizations :: Vector{Vector{NTuple{N, Float64}}}, frame_offsets :: Vector{Int64},
+     max_dist, max_iters; drift = [ntuple(i->0.0, Val(N)) for _ in 1:length(localizations)], lambda = 0.0, recompute_max_dist = 1E-3) where {N}
+    @assert N == 2 || N == 3
     frames = [Frame(locs, d, CellList(locs, max_dist)) for (locs, d) in zip(localizations, drift)]
     cache = indexpairs(length(localizations), frame_offsets) |> Map(((i,j),) -> (i,j,compute_ave_offset(frames[i], frames[j]))) |> tcollect
 
     old_drift = drift
 
     for i in 1:max_iters
-        delta_drift = sum(sum(abs2, d - nd) for (d, nd) in zip(drift, old_drift)) / length(drift)
+        delta_drift = sum(_squared_dist(d, nd) for (d, nd) in zip(drift, old_drift)) / length(drift)
 
         if i != 1 && delta_drift <= 1E-7
             break
@@ -138,7 +145,7 @@ function drift_estimation(localizations :: Vector{Vector{P2D}}, frame_offsets ::
         old_drift = drift
         drift = minimize_wrt_drift(DriftLoss(cache,lambda), drift)
     end
-    [[z - d for z in f] for (f,d) in zip(localizations,drift)], drift
+    [[z .- d for z in f] for (f,d) in zip(localizations,drift)], drift
 end
 
 end
--- src/celllists.jl
+++ src/celllists.jl
@@ -1,49 +1,79 @@
 #### Cell lists. Super simple...
 @inline _bin_idx(x :: Float64, bin_width :: Float64) = ceil(Int64, x/bin_width)
 
-struct CellList
-    cells :: Dict{NTuple{2, Int64}, Vector{P2D}}
-    radius :: Float64
-    CellList(r) = new(Dict(), r)
+@inline function _squared_dist(p,n)
+    r = 0.0
+    for i in 1:length(p)
+        r += (p[i]-n[i])^2
+    end
+    r
 end
 
-function Base.push!(t :: CellList, p :: P2D)
-    k = Tuple(_bin_idx.(p, t.radius))
+struct CellList{N}
+    cells :: Dict{NTuple{N, Int64}, Vector{NTuple{N, Float64}}}
+    radius :: Float64
+    function CellList{N}(r) where {N}
+        @assert N == 2 || N == 3
+        new{N}(Dict(), r)
+    end
+end
+
+function Base.push!(t :: CellList{N}, p :: NTuple{N, Float64}) where {N}
+    k = _bin_idx.(p, t.radius)
+
     list = if k ∉ keys(t.cells)
-        l = P2D[]
-        t.cells[k] = l
+        t.cells[k] = NTuple{N, Float64}[]
     else
         t.cells[k]
     end
 
-    push!(t.cells[k], p)
+    push!(list, p)
 end
 
-function CellList(points :: Vector{P2D}, radius :: Float64)
-    t = CellList(radius)
+function CellList(points :: Vector{NTuple{N, Float64}}, radius :: Float64) where {N}
+    t = CellList{N}(radius)
     for p in points
         push!(t, p)
     end
     t
 end
 
-function neighbor(t :: CellList, p :: P2D)
+# TODO: Remove duplicate code.
+function neighbor(t :: CellList{2}, p :: NTuple{2, Float64})
     r_sq = t.radius*t.radius
     offsets = (0,-1,1)
 
-    i_x = _bin_idx(p[1], t.radius)
-    i_y = _bin_idx(p[2], t.radius)
+    bin_idx = _bin_idx.(p, t.radius)
 
     for o_x in offsets, o_y in offsets
-        k = (i_x + o_x, i_y + o_y)
+        k = bin_idx .+ (o_x, o_y)
         if k ∈ keys(t.cells)
             for n in t.cells[k]
-                d = p-n
-                if dot(d, d) <= r_sq
+                if _squared_dist(p,n) <= r_sq
                     return true, n
                 end
             end
         end
     end
-    return false, P2D(Inf,Inf)
+    return false, (Inf,Inf)
+end
+
+
+function neighbor(t :: CellList{3}, p :: NTuple{3, Float64})
+    r_sq = t.radius*t.radius
+    offsets = (0,-1,1)
+
+    bin_idx = _bin_idx.(p, t.radius)
+
+    for o_x in offsets, o_y in offsets, o_z in offsets
+        k = bin_idx .+ (o_x, o_y, o_z)
+        if k ∈ keys(t.cells)
+            for n in t.cells[k]
+                if _squared_dist(p,n) <= r_sq
+                    return true, n
+                end
+            end
+        end
+    end
+    return false, (Inf,Inf,Inf)
 end
